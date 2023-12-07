cbdc031a90cb5997ac934a9374b238dc41fe415a
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-12-07 18:08:29 +0200

    yy

--- java.js
+++ java.js
@@ -4,6 +4,38 @@ const sliderArrItems = Array.from(sliderItems);
 
 const btnNext = document.querySelector(".btnNext");
 const btnNextNoNext = document.querySelector(".btnPrev");
+
+
+
+btnNext.addEventListener("click", _.debounce(() => {
+    const curentSlide = slider.querySelector("[data-active]");
+    const curentSlideIndex = +curentSlide.dataset.index;
+ 
+    curentSlide.classList.add("hidden");
+    curentSlide.removeAttribute("data-active");
+    
+    const nextSlideIndex = curentSlideIndex + 1 === sliderArrItems.length ? 0 : curentSlideIndex + 1;
+    const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+    nextSlide.classList.remove("hidden");
+    nextSlide.setAttribute("data-active", "");
+
+  }, 1000))
+  
+
+
+  btnNextNoNext.addEventListener("click", _.debounce(() => {
+    const curentSlide = slider.querySelector("[data-active]");
+    const curentSlideIndex = +curentSlide.dataset.index;
+    
+    curentSlide.classList.add("hidden");
+    curentSlide.removeAttribute("data-active");
+    
+    const nextSlideIndex = curentSlideIndex  === 0 ? sliderArrItems.length - 1 : curentSlideIndex - 1;
+    const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+    nextSlide.classList.remove("hidden");
+    nextSlide.setAttribute("data-active", "");
+  }, 1000))
+
 sliderArrItems.forEach(function (slide, index) {
    
     if (index !== 0) {
@@ -27,34 +59,6 @@ nextSlide.setAttribute("data-active", "")
 })
 
 
-btnNext.addEventListener("click", nextSlide)
-btnNextNoNext.addEventListener("click", nexNotSlide)
-
-function nextSlide() {
-    const curentSlide = slider.querySelector("[data-active]");
-const curentSlideIndex = +curentSlide.dataset.index;
-
-curentSlide.classList.add("hidden");
-curentSlide.removeAttribute("data-active");
-
-const nextSlideIndex = curentSlideIndex + 1 === sliderArrItems.length ? 0 : curentSlideIndex + 1;
-const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-nextSlide.classList.remove("hidden");
-nextSlide.setAttribute("data-active", "");
-}
-
-function nexNotSlide() {
-    const curentSlide = slider.querySelector("[data-active]");
-    const curentSlideIndex = +curentSlide.dataset.index;
-    
-    curentSlide.classList.add("hidden");
-    curentSlide.removeAttribute("data-active");
-    
-    const nextSlideIndex = curentSlideIndex  === 0 ? sliderArrItems.length - 1 : curentSlideIndex - 1;
-    const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-    nextSlide.classList.remove("hidden");
-    nextSlide.setAttribute("data-active", "");
-}
 
 
 
